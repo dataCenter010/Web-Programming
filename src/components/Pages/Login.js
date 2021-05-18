8be2caf884d7b63cfce30df293fc22afa9c80667
OkenuJulietU <j2okenu@gmail.com>
2021-05-18 21:38:20 +0100

    auth design completed

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './style.css'
 
 
  const Login = () =>{
@@ -6,6 +7,43 @@ import React from 'react'
         <main className='container'>
             <section>
                 <h1>Login Page</h1>
+                <div id="wrapper">
+    <div id="header">
+    	 <h1>CSC506 Web & Internet Programming</h1>
+    </div>
+            <div className="login-wrap">
+            <div className="login-html">
+            <input id="tab-1" type="radio" name="tab" className="sign-in" checked>
+            <label for="tab-1" className="tab">Login</label>
+            <input id="tab-2" type="radio" name="tab" className="sign-up">
+            <div className="login-form">
+            <div className="sign-in-htm"> 
+            <div className="group">
+            <label for="user" className="label">Username</label>
+            <input id="user" type="text" className="input">
+            </div> 
+            <div className="group">
+            <label for="pass" className="label">Password</label>
+            <input id="pass" type="password" className="input" data-type="password">
+            </div>
+            <div className="group">
+            <input id="check" type="checkbox" className="check" checked>
+            <label for="check">
+            <span className="icon"></span> Keep me Logged in</label> 
+            </div>
+            <div className="group">
+            <a href="home.html"><input type="submit" className="button" value="Login"></a>
+            </div>
+            <div className="hr"></div>
+            <div className="foot-lnk">
+            <a href="#forgot">Forgot Password?</a>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            
+                </div>
             </section>
         
         </main>
--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './style.css';
 
 
  const SignUp = () =>{
@@ -6,6 +7,41 @@ import React from 'react'
         <main className='container'>
             <section>
                 <h1>SignUp Page</h1>
+                <div id="wrapper">
+    <div id="header">
+    	 <h1>CSC506 Web & Internet Programming</h1>
+    </div>
+                <label for="tab-2" className="tab">Sign Up</label>
+                <div className="sign-up-htm">
+ <div className="group">
+ <label for="user" className="label">Username</label>
+ <input id="user" type="text" className="input">
+ </div>
+ <div className="group">
+ <label for="pass" className="label">Password</label>
+ <input id="pass" type="password" className="input" data-type="password">
+ </div>
+ <div className="group">
+ <label for="pass" className="label">Repeat Password</label>
+ <input id="pass" type="password" className="input" data-type="password">
+ </div>
+ <div className="group">
+ <label for="pass" className="label">Email Address</label>
+ <input id="pass" type="text" className="input">
+ </div>
+ <div className="group">
+ <a href="home.html"><input type="submit" className="button" value="Register"></a>
+ </div>
+ <div className="hr"></div>
+ <div className="foot-lnk">
+ <label for="tab-1">Already Member?</a>
+ </div>
+</div>
+<div id="footer">
+	<center>Copyright &copy; All rights reserved, CSC506: Web & Internet Programming</center>
+</div>
+</div>
+
             </section>
         
         </main>
